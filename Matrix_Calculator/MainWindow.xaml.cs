6faf5f7c08254fc193cd60dd7e7b872419633e83
houman ebrahimi <houmanebrahimi67@gmail.com>
2022-06-14 19:22:40 -0400

    added some stuff

--- Matrix_Calculator/MainWindow.xaml.cs
+++ Matrix_Calculator/MainWindow.xaml.cs
@@ -36,9 +36,12 @@ namespace Matrix_Calculator
         List<TextBox> textBox = new List<TextBox>(1000);
         List<TextBox> textBox2=new List<TextBox>(1000);
 
+        List<TextBox> resultBox=new List<TextBox>(1000);
+
         int row, col, row2, col2;
 
 
+        string operation = "";
 
         public MainWindow()
         {
@@ -84,8 +87,8 @@ namespace Matrix_Calculator
 
            // AllocConsole();
 
-            int row = int.Parse(Row.Text);
-            int col = int.Parse(Col.Text);
+             row = int.Parse(Row.Text);
+             col = int.Parse(Col.Text);
 
 
             for(int i=0; i<row*col; i++)
@@ -145,12 +148,13 @@ namespace Matrix_Calculator
         private void test(object sender, RoutedEventArgs e)
         {
             int result = 0;
+            int result2 = 0;
             int index = 0;
             AllocConsole();
             for (int i = 0; i < textBox.Count; i++)
             {
-                if (Int32.TryParse(textBox[i].Text,out result)){
-                    Console.WriteLine(int.Parse(textBox[i].Text));
+                if (Int32.TryParse(textBox[i].Text,out result) && Int32.TryParse(textBox2[i].Text,out result2)){
+                    Console.WriteLine((int.Parse(textBox[i].Text),int.Parse(textBox2[i].Text)));
                 }
             }
 
@@ -160,7 +164,6 @@ namespace Matrix_Calculator
         private void Change2(object sender, RoutedEventArgs e)
         {
             int index = 0;
-            //AllocConsole();
 
              row2 = int.Parse(Row2.Text);
              col2 = int.Parse(Col2.Text);
@@ -216,9 +219,162 @@ namespace Matrix_Calculator
 
         private void Add(object sender, RoutedEventArgs e)
         {
+            int result = 0;
+            int two = 0;
+            int index = 0;
+
+            int rowResult = row2;
+            int colResult=col2;
+            
+            for (int i = 0; i < textBox.Count; i++)
+            {
+                if (int.TryParse(textBox[i].Text, out result) && int.TryParse(textBox2[i].Text,out two))
+                {
+                    resultBox.Add(new TextBox());
+                    int val = int.Parse(textBox[i].Text) + int.Parse(textBox2[i].Text);
+                    resultBox[i].Text=val.ToString();
+
+
+                }
+            }
+
+            for (int i = 0; i < rowResult * colResult; i++)
+            {
+                resultBox.Add(new TextBox());
+
+
+            }
+
+            for (int i = 0; i < rowResult; i++)
+            {
+
+                for (int j = 0; j < colResult; j++)
+                {
+                    Grid.SetRow(resultBox[index], i);
+
+                    Grid.SetColumn(resultBox[index], j);
+
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < rowResult * colResult; i++)
+            {
+                grid3.Children.Add(resultBox[i]);
+
+
+            }
+
+            for (int i = 0; i <rowResult; i++)
+            {
+
+                rowDef1 = new RowDefinition();
+
+                grid3.RowDefinitions.Add(rowDef1);
+
+            }
+
+            for (int i = 0; i < colResult; i++)
+            {
+                colDef1 = new ColumnDefinition();
+
+                grid3.ColumnDefinitions.Add(colDef1);
+
+            }
+
+
 
         }
 
+        private void Subtract (object sender, RoutedEventArgs e)
+        {
+            int result = 0;
+            int two = 0;
+            int index = 0;
+
+            int rowResult = row2;
+            int colResult = col2;
+
+            for (int i = 0; i < textBox.Count; i++)
+            {
+                if (int.TryParse(textBox[i].Text, out result) && int.TryParse(textBox2[i].Text, out two))
+                {
+                    resultBox.Add(new TextBox());
+                    int val = int.Parse(textBox[i].Text) - int.Parse(textBox2[i].Text);
+                    resultBox[i].Text = val.ToString();
+
+
+                }
+            }
+
+            for (int i = 0; i < rowResult * colResult; i++)
+            {
+                resultBox.Add(new TextBox());
+
+
+            }
+
+            for (int i = 0; i < rowResult; i++)
+            {
+
+                for (int j = 0; j < colResult; j++)
+                {
+                    Grid.SetRow(resultBox[index], i);
+
+                    Grid.SetColumn(resultBox[index], j);
+
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < rowResult * colResult; i++)
+            {
+                grid3.Children.Add(resultBox[i]);
+
+
+            }
+
+            for (int i = 0; i < rowResult; i++)
+            {
+
+                rowDef1 = new RowDefinition();
+
+                grid3.RowDefinitions.Add(rowDef1);
+
+            }
+
+            for (int i = 0; i < colResult; i++)
+            {
+                colDef1 = new ColumnDefinition();
+
+                grid3.ColumnDefinitions.Add(colDef1);
+
+            }
+
+
+        }
+
+        private void Multiply(object sender, RoutedEventArgs e)
+        {
+
+            int rowResult = 0;
+            int colResult = 0;
+
+            if (col == row2)
+            {
+                try
+                {
+                    rowResult = row;
+                    colResult = col2;
+                }
+                catch (Exception ex)
+                {
+                    ExitEventArgs();
+                }
+            }
+
+
+        }
     private void to_TextChanged(object sender, TextChangedEventArgs e)
         {
 
